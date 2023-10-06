19876df79704f93e60b352a31745d7343c1e37ed
RAJ MANDAL <usingrajcseinfo@gmail.com>
2023-10-07 03:03:05 +0530

    add styling

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 
-
+const wrapper = document.querySelector('.wrapper');
 
 
 
@@ -12,3 +12,16 @@ setTimeout(() => {
     
    load.style.display="none";
 }, 3000);
+
+function changeColor(){
+   const red = Math.floor(Math.random() * 256);
+   const green = Math.floor(Math.random() * 256);
+   const blue = Math.floor(Math.random() * 256);
+   return `rgb(${red},${green},${blue})`;
+}
+
+setInterval(()=>{
+   // console.log(changeColor());
+   wrapper.style.backgroundColor = changeColor();
+}, 1000);
+// changeColor();
